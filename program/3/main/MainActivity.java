7eb61d12d68f5a7628d79f5bdb18a685b9b16fd6
K.R SAI VARUN <krsaivarun@gmail.com>
2025-05-19 11:36:08 +0530

    Create MainActivity.java

---
+++ program/3/main/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextUsername,editTextPassword;
+    private Button buttonLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                if(username.equals("admin") && password.equals("pass")){
+                    Toast.makeText(MainActivity.this, "Login successful",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid username or password",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
