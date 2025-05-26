dea405b34e3f56072c17211945b1c3d44b241531
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2025-05-26 12:18:46 +0530

    Update MainActivity.java
    reduce the lines

--- program/2/MainActivity.java
+++ program/2/MainActivity.java
@@ -1,37 +1,31 @@
 package com.example.myapplication2;
+
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.myapplication2.EdgeToEdge;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button l,p;
-        l=findViewById(R.id.lan);
-        p=findViewById(R.id.por);
-        l.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                Toast.makeText(MainActivity.this, "Hey! We are in Landscape  orientation", Toast.LENGTH_SHORT).show();
-            }
+
+        Button l = findViewById(R.id.lan);
+        Button p = findViewById(R.id.por);
+
+        l.setOnClickListener(v -> {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            Toast.makeText(this, "Hey! We are in Landscape orientation", Toast.LENGTH_SHORT).show();
         });
-        p.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-                Toast.makeText(MainActivity.this, "Hey! We are in Portrait orientation",
-                        Toast.LENGTH_SHORT).show();
-            }
+
+        p.setOnClickListener(v -> {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            Toast.makeText(this, "Hey! We are in Portrait orientation", Toast.LENGTH_SHORT).show();
         });
     }
 }
