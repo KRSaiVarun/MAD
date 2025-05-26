c08136b3395a648b959361c4d5a5a6cc889a900c
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2025-05-26 14:31:41 +0530

    Update MainActivity.java

--- program/3/main/MainActivity.java
+++ program/3/main/MainActivity.java
@@ -9,28 +9,26 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editTextUsername,editTextPassword;
+    private EditText editTextUsername, editTextPassword;
     private Button buttonLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         editTextUsername = findViewById(R.id.editTextUsername);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String username = editTextUsername.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-                if(username.equals("admin") && password.equals("pass")){
-                    Toast.makeText(MainActivity.this, "Login successful",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Invalid username or password",
-                            Toast.LENGTH_SHORT).show();
-                }
+
+        buttonLogin.setOnClickListener(v -> {
+            String username = editTextUsername.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+            if ("admin".equals(username) && "pass".equals(password)) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
             }
         });
     }
