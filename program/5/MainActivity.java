4cf1379209d4fe31aa89d7085a1329f5d18dff89
K.R SAI VARUN <krsaivarun@gmail.com>
2025-05-29 01:33:33 +0530

    Create MainActivity.java

---
+++ program/5/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pgm3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, MainScreen.class));
+            finish();
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
