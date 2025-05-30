4f8cdacf5207c53c89c0e7fe2ad14ea327ffebf9
K.R SAI VARUN <krsaivarun@gmail.com>
2025-05-30 11:03:08 +0530

    Update MainActivity.java

--- program/5/MainActivity.java
+++ program/5/MainActivity.java
@@ -1,21 +1,34 @@
-package com.example.pgm3;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000; // 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set layout for splash screen
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(this, MainScreen.class));
-            finish();
+
+        // Make activity full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Transition to mainScreen after delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, mainScreen.class);
+                startActivity(i);
+                finish();
+            }
         }, SPLASH_SCREEN_TIME_OUT);
     }
 }
