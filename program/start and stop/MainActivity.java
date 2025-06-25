bc1765c389ce7dbb97c94c84e6ebb4f0ec3cfd1a
K.R SAI VARUN <krsaivarun@gmail.com>
2025-06-25 15:59:10 +0530

    Update MainActivity.java

--- program/start and stop/MainActivity.java
+++ program/start and stop/MainActivity.java
@@ -3,31 +3,27 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView counterText;
-    private Handler handler = new Handler();
-    private int counter = 0;
+    private TextView counterText; 
+    private Handler handler = new Handler(); 
+    private int counter = 0; 
     private boolean isCounting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        counterText = findViewById(R.id.counterText);
+        counterText = findViewById(R.id.counterTextView);
         findViewById(R.id.startButton).setOnClickListener(v -> startCounting());
         findViewById(R.id.stopButton).setOnClickListener(v -> isCounting = false);
     }
 
     private void startCounting() {
-        isCounting = true;
+        isCounting = true; 
         handler.postDelayed(new Runnable() {
-            @Override
             public void run() {
                 if (isCounting) {
                     counterText.setText("Counter: " + ++counter);
