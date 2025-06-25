bb8e385e063a40dbcf683f769fb53cb6f4efe11f
K.R SAI VARUN <krsaivarun@gmail.com>
2025-06-25 13:53:45 +0530

    Update MainActivity.java

--- program/4/MainActivity.java
+++ program/4/MainActivity.java
@@ -4,27 +4,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
     }
-    public void onImplicitButtonClicked(View view)
-    {
-        Uri url=Uri.parse("https://www.google.com");
-        Intent i=new Intent(Intent.ACTION_VIEW, url);
-        startActivity(i);
+
+    public void onImplicitButtonClicked(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com")));
     }
-    public void onExplicitButtonClicked(View view )
-    {
-        Intent i=new Intent(MainActivity.this, MainActivity.class);
-        startActivity(i);
+
+    public void onExplicitButtonClicked(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
