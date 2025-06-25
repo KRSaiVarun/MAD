5ebf18558be98e88ccea9ddf677beea678728cb0
K.R SAI VARUN <krsaivarun@gmail.com>
2025-06-25 13:36:41 +0530

    Update MainActivity.java

--- program/1st/MainActivity.java
+++ program/1st/MainActivity.java
@@ -5,18 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b;
-        b=findViewById(R.id.hello);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hey! We are using Android Application", Toast.LENGTH_SHORT).show();
-            }
-        });
+        Button b = findViewById(R.id.hello);
+        b.setOnClickListener(v -> 
+            Toast.makeText(this, "Hey! We are using Android Application", Toast.LENGTH_SHORT).show());
     }
 }
