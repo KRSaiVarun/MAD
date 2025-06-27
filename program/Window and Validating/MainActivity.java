ebf81075bba8506c3f6fa1cb20bb5fa85925bc08
K.R SAI VARUN <krsaivarun@gmail.com>
2025-06-27 15:28:36 +0530

    Create MainActivity.java

---
+++ program/Window and Validating/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.loginapp;
+
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.TextView; 
+import android.widget.Toast; 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText usernameInput, passwordInput; 
+    private TextView loginStatus;
+    private static final String USER = "user", PASS = "password";
+
+    @Override 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.activity_main);
+        usernameInput = findViewById(R.id.usernameInput); 
+        passwordInput = findViewById(R.id.passwordInput); 
+        loginStatus = findViewById(R.id.loginStatus);
+        
+        findViewById(R.id.loginButton).setOnClickListener(v -> validateLogin());
+    }
+
+    private void validateLogin() {
+        if (usernameInput.getText().toString().equals(USER) && passwordInput.getText().toString().equals(PASS)) {
+            loginStatus.setText("Login Successful");
+        } else {
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
