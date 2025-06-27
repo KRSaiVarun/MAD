d2a543f8f3dc1e6d2e4b5ad6e9e1ea8546661924
K.R SAI VARUN <krsaivarun@gmail.com>
2025-06-27 15:46:26 +0530

    Update MainActivity.java

--- program/1st/MainActivity.java
+++ program/1st/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Ensure this matches your XML file name
+        setContentView(R.layout.activity_main); 
         Button b = findViewById(R.id.hello);
         b.setOnClickListener(v ->
                 Toast.makeText(this, "Hey! We are using Android Application", Toast.LENGTH_SHORT).show());
