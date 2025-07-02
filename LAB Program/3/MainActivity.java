1f89df7526f8308ff08c5b23f34cea87e950ea65
K.R SAI VARUN <krsaivarun@gmail.com>
2025-07-02 15:14:43 +0530

    Create MainActivity.java

---
+++ LAB Program/3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import android.content.pm.*;
+import android.os.*;
+import android.widget.*;
+import androidx.appcompat.app.*;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button por = findViewById(R.id.por);
+        Button lan = findViewById(R.id.lan);
+
+        lan.setOnClickListener(v -> changeOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE, "Hey! We are in Landscape orientation"));
+        por.setOnClickListener(v -> changeOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT, "Hey! We are in Portrait orientation"));
+    }
+
+    private void changeOrientation(int orientation, String message) {
+        setRequestedOrientation(orientation);
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
